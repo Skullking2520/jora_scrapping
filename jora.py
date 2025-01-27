be4f192ff91cd8c7ada89999cbfc62ab1b636b5b
Skullking2520 <hyunjunsoo1@gmail.com>
2025-01-27 16:58:50 +0800

    Update jora.py

--- jora.py
+++ jora.py
@@ -17,6 +17,7 @@ options.add_argument("--headless")
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-extensions")
+options.add_argument('--start-maximized')
 driver = webdriver.Chrome(options=options)
 
 def main():
