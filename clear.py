8c502a94e046012efb408dea2e6a618f71f93167
Skullking2520 <hyunjunsoo1@gmail.com>
2025-03-06 16:08:06 +0900

    Create clear.py

---
+++ clear.py
@@ -0,0 +1,21 @@
+import json
+
+from google_form_package import Sheet
+
+
+def main():
+    web_sheet = Sheet()
+    sheet1 = web_sheet.get_worksheet("Sheet1")
+    sheet2 = web_sheet.get_worksheet("Sheet2")
+    progress_sheet = web_sheet.get_worksheet("Progress")
+    sheet1.update([["Scrapping Finished"]], "Q1")
+    sheet2.update([["Reporting Finished"]], "E1")
+    process_sheet.update("A1", [[json.dumps({"progress":"setting", "UrlNum": 1})]])
+    process_sheet.update("A2", [[json.dumps({"progress":"setting", "RowNum": 0})]])
+    process_sheet.update("B2", [[json.dumps({"progress":"setting", "RowNum": 1})]])
+    process_sheet.update("C2", [[json.dumps({"progress":"setting", "RowNum": 2})]])
+    process_sheet.update("D2", [[json.dumps({"progress":"setting", "RowNum": 3})]])
+    process_sheet.update("E2", [[json.dumps({"progress":"setting", "RowNum": 4})]])
+  
+if __name__ == "__main__":
+    main()
